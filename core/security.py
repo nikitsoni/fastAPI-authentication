5675493325f722fe6eee25c6a56e649970ee8e03
Nikit Soni <nikitsoni17@gmail.com>
2024-06-27 01:13:29 +0530

    Authorised Route Created

--- core/security.py
+++ core/security.py
@@ -1,8 +1,12 @@
+from fastapi import Depends
 from passlib.context import CryptContext
 from fastapi.security import OAuth2PasswordBearer
 from datetime import datetime, timedelta, timezone
 from core.config import get_settings
 from jose import jwt, JWTError
+from starlette.authentication import AuthCredentials, UnauthenticatedUser
+from core.database import get_db
+from users.models import UserModel
 
 settings = get_settings()
 
@@ -30,3 +34,38 @@ def get_token_payload(token):
 
 async def create_refresh_token(data):
     return jwt.encode(data, settings.JWT_SECRET, algorithm=settings.JWT_ALGORITHM)
+
+def get_current_user(token: str = Depends(oauth2_scheme), db = None):
+    payload = get_token_payload(token)
+    if not payload or type(payload) is not dict:
+        return None
+    
+    user_id = payload.get('id', None)
+    if not user_id:
+        return None
+    
+    if not db:
+        db = next(get_db())
+    
+    user = db.query(UserModel).filter(UserModel.id == user_id).first()
+    return user
+    
+
+class JWTAuth:
+    
+    async def authenticate(self, conn):
+        guest = AuthCredentials(['unauthenticated']), UnauthenticatedUser()
+        
+        if 'authorization'not in conn.headers:
+            return guest
+        
+        token = conn.headers['authorization'].split(' ')[1]
+        if not token:
+            return guest
+        
+        user = get_current_user(token=token)
+        
+        if not user:
+            return guest
+        
+        return AuthCredentials('authenticated'), user
--- main.py
+++ main.py
@@ -1,11 +1,16 @@
 from fastapi import FastAPI
 from fastapi.responses import JSONResponse
-from users.routes import router as user_router
+from users.routes import router as guest_router, user_router
 from auth.routes import router as auth_router
+from core.security import JWTAuth
+from starlette.middleware.authentication import AuthenticationMiddleware
 
 app = FastAPI()
+app.include_router(guest_router)
 app.include_router(user_router)
 app.include_router(auth_router)
+
+app.add_middleware(AuthenticationMiddleware, backend=JWTAuth())
 
 @app.get('/')
 def heath_check():
